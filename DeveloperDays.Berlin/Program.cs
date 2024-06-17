1b4e8b8580cf66fc7680e8b241b34b57134129f6
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-06-17 18:27:28 +0200

    INFRA: Init Api Project

--- DeveloperDays.Berlin/Program.cs
+++ DeveloperDays.Berlin/Program.cs
@@ -1,17 +1,36 @@
-﻿// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 // Copyright (c) 2024 eBiz Consulting GmbH
 // Made w/ love by Mabrouk Mahdhi for all .NET developer days attendees
 // ---------------------------------------------------------------------
 
-using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DeveloperDays.Berlin
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
         }
     }
 }
