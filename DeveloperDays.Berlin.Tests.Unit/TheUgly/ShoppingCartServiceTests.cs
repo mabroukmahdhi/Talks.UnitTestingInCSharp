d3f158bcf9ff7edd7247bc93057df021576399e6
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-06-18 00:08:38 +0200

    TheUgly

---
+++ DeveloperDays.Berlin.Tests.Unit/TheUgly/ShoppingCartServiceTests.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------------
+// Copyright (c) 2024 eBiz Consulting GmbH
+// Made w/ love by Mabrouk Mahdhi for all .NET developer days attendees
+// ---------------------------------------------------------------------
+
+using DeveloperDays.Berlin.Services;
+
+namespace DeveloperDays.Berlin.Tests.Unit.TheUgly
+{
+    public class ShoppingCartServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var service = new ShoppingCartService();
+            service.AddItemToCart("item1", 2);
+            var total = service.CalculateTotalPrice();
+            Assert.True(total == 20); // Hardcoded magic number
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var service = new ShoppingCartService();
+            service.AddItemToCart("item1", 1);
+            service.AddItemToCart("item2", 1);
+            var total = service.CalculateTotalPrice();
+            Assert.True(total == 30); // Hardcoded magic number
+        }
+    }
+}
