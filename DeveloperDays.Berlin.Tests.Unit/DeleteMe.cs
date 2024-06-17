620549a79ddc63fa1678402725beea52d6b7bfec
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-06-17 17:35:42 +0200

    INFRA: Initialize Projects

---
+++ DeveloperDays.Berlin.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------------
+// Copyright (c) 2024 eBiz Consulting GmbH
+// Made w/ love by Mabrouk Mahdhi for all .NET developer days attendees
+// ---------------------------------------------------------------------
+
+namespace DeveloperDays.Berlin.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
---
+++ DeveloperDays.Berlin/Program.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------
+// Copyright (c) 2024 eBiz Consulting GmbH
+// Made w/ love by Mabrouk Mahdhi for all .NET developer days attendees
+// ---------------------------------------------------------------------
+
+using System;
+
+namespace DeveloperDays.Berlin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
